bd2dfb7230a79734b1bdf1cdb91bf07323bd8286
AfonsoMartinho <am.devlops@gmail.com>
2023-06-01 13:21:35 +0100

    added a cards grid with api data, pagination component now has totalCount based on cardsListSize

--- src/App.js
+++ src/App.js
@@ -4,27 +4,36 @@ import Card from './components/Card/Card';
 import Dropdown from './components/Dropdown/Dropdown';
 import Grid from './components/Grid/Grid';
 import Pagination from './components/Pagination/Pagination';
+import { useGetCardsQuery } from './services/cardsService';
 
 import './App.css';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
+  const {
+    data: cards = {},
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  }  = useGetCardsQuery({page: currentPage});
+
+  if (isLoading || isFetching) {
+    return <div>loading...</div>;
+  }
+  
+  if (isError) {
+    console.log({ error });
+    return <div>{error.status}</div>;
+  }
+
+  const cardsList = cards.list || [];
+  const cardListSize = cards.size || 0;
+
+  console.log(cards)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
       <div style={{ paddingBlock: '25px' }}></div>
 
@@ -33,35 +42,20 @@ function App() {
       </section>
 
       <div style={{ paddingBlock: '25px' }}></div>
-
+      
       <Grid>
-        <Card />
-        <Card />
-
-        <Card />
-        <Card />
-
-        <Card />
-        <Card />
-
-        <Card />
-        <Card />
-
-        <Card />
-        <Card />
-
-        <Card />
-        <Card />
+      {cardsList.map((card,i) => (
+        <Card key={`${card.title}-${i}`} card={card}/>
+      ))}
       </Grid>
-
       <div style={{ paddingBlock: '25px' }}></div>
 
       <div style={{ marginInline: '10%' }}>
         <Pagination
           className="pagination-bar"
           currentPage={currentPage}
-          totalCount={100}
-          pageSize={16}
+          totalCount={cardListSize}
+          pageSize={20}
           onPageChange={page => setCurrentPage(page)}
         />
       </div>
