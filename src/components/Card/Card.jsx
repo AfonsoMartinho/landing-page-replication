086ea3cbd5661363ba1640fc867230bc8d59d73d
AfonsoMartinho <am.devlops@gmail.com>
2023-06-04 23:17:15 +0100

    feat: added styling of Cards

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import { StyledCard, StyledCardLink, StyledCardMedia, StyleCardImage, StyleCardLabel, StyledCardH5 } from './Card.styled'
+
+const Card = ({card}) => {
+  const { image, label, title, content, link } = card
+  console.log(card)
+  return (
+    <StyledCard>
+      <StyledCardLink traget={link.target} href={link.url}>
+        <StyledCardMedia>
+          <StyleCardImage {...image} />
+        </StyledCardMedia>
+      </StyledCardLink>
+
+      <StyledCardLink traget={link.target} href={link.url}>
+        <span title={label.text}>
+          <StyleCardLabel>{label.text}</StyleCardLabel>
+        </span>
+
+        <StyledCardH5 className='card__title'>
+          {title}
+        </StyledCardH5>
+
+        <div className="card__content">{content?.text}</div>
+      </StyledCardLink>
+      
+    </StyledCard >
+  )
+}
+
+export default Card
