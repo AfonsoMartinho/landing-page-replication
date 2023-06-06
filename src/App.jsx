03f6aac35edde44483d31190fcdbc190af63f1fd
AfonsoMartinho <am.devlops@gmail.com>
2023-06-06 13:49:40 +0100

    feat: removed index.css file and added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Costumers from './views/Costumers';
 
 function App() {
   return (
-    <div className="costumers-page">
+    <div className="costumers-page">   
       <div style={{ paddingBlock: '25px' }}></div>
       <section>
         <Costumers/>
